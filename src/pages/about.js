d85649b99bc6de2f3a9bc8b5d6af59d7e15cc1d5
AliTaee <ali.taee.72@gmail.com>
2020-04-30 20:55:25 +0430

    Add about page and change index home page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About</h1>
+    <p>I'm a Front-End Developer.</p>
+  </Layout>
+)
+
+export default AboutPage
