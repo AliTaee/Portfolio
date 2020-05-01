6301314c3c8470418b15cf058de86906960c26bc
AliTaee <ali.taee.72@gmail.com>
2020-05-01 12:37:45 +0430

    Change heading page to h2, we have h1 in navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="خانه" />
-    <h1>خانه</h1>
+    <h2>خانه</h2>
     <p>علی طائی</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
