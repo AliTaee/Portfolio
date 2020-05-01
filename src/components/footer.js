dcd1c541f4418946380ed0e867fe8dff0d21b103
AliTaee <ali.taee.72@gmail.com>
2020-05-01 12:44:11 +0430

    Improve footer style and emoji accessibility

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Emoji from "../components/emoji"
 
 const Footer = () => (
-  <footer>
-    © {new Date().getFullYear()}, ساخته شده با
+  <footer className="footer">
+    © {new Date().getFullYear()} | ساخته شده با{" "}
+    <Emoji emoji="❤️" label="love" />، <Emoji emoji="☕" label="coffee" /> و
     {` `}
     <a
+      className="footer__gatsbyjs"
       href="https://www.gatsbyjs.org"
       target="_blank"
       rel="noopener noreferrer"
