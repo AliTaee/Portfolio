b9c4cca2b4fcc5d54627d1efdcf2af9b22bcfc02
AliTaee <ali.taee@zoodfood.com>
2021-04-01 11:39:07 +0430

    Add about me and photo to resume section

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,9 +1,10 @@
 import React from "react"
-import SEO from "../components/seo"
-import Layout from "../components/layout/layout"
 
 // Resume component
+import SEO from "../components/seo"
+import Image from "../components/common/image"
 import Badge from "../components/resume/badge"
+import Layout from "../components/layout/layout"
 import CategoryTitle from "../components/resume/category-title"
 
 const Resume = () => (
@@ -15,6 +16,16 @@ const Resume = () => (
       <CategoryTitle title="certificates" />
     </article>
     <aside className="resume__sidebar">
+      <Image className="img-round resume__img" />
+      <p className="resume__about">
+        I am a Front-end and WordPress developer who started learning Web
+        programming back in 2016. Reading the Web sites practicing and learning
+        through the implementation of the projects and new challenges also my
+        daily habits. My career passion is JavaScript, React and user interface
+        and my interests are reading book, music and photography. The strengths
+        of my work are learning, ideas, user experience (UX) and user interface
+        (UI).
+      </p>
       <CategoryTitle title="skills" />
       <Badge title="React" />
       <Badge title="JavaScript" />
