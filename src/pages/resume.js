819b101d5402de8129e3960351a34c9554d6cd60
AliTaee <ali.taee@zoodfood.com>
2021-04-06 20:51:46 +0430

    Add resume experience and code wars section

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,16 +5,29 @@ import SEO from "../components/seo"
 import Image from "../components/common/image"
 import Badge from "../components/resume/badge"
 import Layout from "../components/layout/layout"
+
+import ResumeSection from "../components/resume/resume-section"
 import CategoryTitle from "../components/resume/category-title"
 
 // Resume source
-import { skills } from "../resume-source"
+import { skills, workExperience } from "../resume-source"
 
 const Resume = () => (
   <Layout classStyle="flex row-reverse">
     <SEO title="Resume" />
     <article className="resume__main">
       <CategoryTitle title="work experience" />
+      <ul className="unbulleted-list">
+        {workExperience.map(experience => (
+          <ResumeSection
+            key={experience.id}
+            list={experience.list}
+            time={experience.time}
+            mainTitle={experience.mainTitle}
+            secondaryTitle={experience.secondaryTitle}
+          />
+        ))}
+      </ul>
       <CategoryTitle title="projects" />
       <CategoryTitle title="certificates" />
     </article>
@@ -29,6 +42,12 @@ const Resume = () => (
         of my work are learning, ideas, user experience (UX) and user interface
         (UI).
       </p>
+      <img
+        src="https://camo.githubusercontent.com/eb8b521f96683fe87aab7b8c510b135037b0687cc0d20a1997a1d81fe373cc12/68747470733a2f2f7777772e636f6465776172732e636f6d2f75736572732f416c69546165652f6261646765732f6c61726765"
+        alt="my profile at codewars"
+        data-canonical-src="https://www.codewars.com/users/AliTaee/badges/large"
+        style={{ maxWidth: "100%" }}
+      ></img>
       <CategoryTitle title="skills" />
       <article className="resume__skills">
         {skills.map(skill => (
