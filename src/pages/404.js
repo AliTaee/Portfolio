7f5409aa4e4198a9b37f868611e3e5e760dc4094
AliTaee <ali.taee@zoodfood.com>
2021-04-08 18:09:16 +0430

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
 import React from "react"
 
-import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+import Emoji from "../components/common/emoji"
+import Layout from "../components/layout/layout"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout classStyle="page-404">
     <SEO title="404 Not found" />
-    <h1>Not found!</h1>
+    <h1>
+      Not found! <Emoji emoji="😔" label="sad" />
+    </h1>
     <p>Look like you are lost or I can't find what you wanted.</p>
   </Layout>
 )
