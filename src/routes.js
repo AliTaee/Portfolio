854fa0b2dfeff63285955d1730874ef3814a8f7c
AliTaee <ali.taee@zoodfood.com>
2021-05-14 20:45:35 +0430

    Create file for routes

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+export const routes = [
+  { id: 0, label: "Home", href: "/" },
+  { id: 1, label: "Resume", href: "/resume/" },
+  { id: 2, label: "About", href: "/about/" },
+  { id: 3, label: "Projects", href: "/projects/" },
+  { id: 4, label: "Snippets", href: "/snippets/" },
+  { id: 5, label: "Blog", href: "/blog/" },
+]
