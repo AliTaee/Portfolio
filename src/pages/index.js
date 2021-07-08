e7bfe79fdb3296b76c423017d7ee27f6222b2860
alitaee <ali.taee.72@gmail.com>
2021-07-08 15:07:33 +0430

    read developer main info dynamically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { developerInfo } from "../resume-source"
+
 import Layout from "../components/layout/layout"
 import Image from "../components/common/image"
 import Emoji from "../components/common/emoji"
@@ -11,23 +13,18 @@ const IndexPage = () => (
     <div className="intro">
       <section className="intro__welcome">
         <h1 className="intro__title">
-          Hello World, I'm Ali Taee{" "}
+          Hello World, I'm{" "}
+          <span className="text-capitalize">{developerInfo.name}</span>
           <span className="hand-wave">
             <Emoji emoji="👋" label="Hand waving" />
           </span>
         </h1>
-        <p>
-          I am a Front-end developer with 4 years of experience. I enjoy every
-          aspect of the job from the implement user interface to advanced
-          JavaScript topics. I have a passion to work on side projects to learn
-          more and create new things. My interests are reading novels, music,
-          and photography.
-        </p>
+        <p>{developerInfo.about}</p>
       </section>
       <section className="card intro__card card--small">
         <Image className="img-round" />
-        <h2 className="card__title">Ali Taee</h2>
-        <p className="card__desc">Front-end developer</p>
+        <h2 className="card__title">{developerInfo.name}</h2>
+        <p className="card__desc">{developerInfo.title}</p>
       </section>
     </div>
   </Layout>
