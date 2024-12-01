b1adbf0b7bdb6b75e03b48edcb99a297413a3570
AliTaee <ali.taee.72@gmail.com>
2024-12-01 19:10:19 +0100

    test fixing speed and loading pages without blog

--- svelte.config.js
+++ svelte.config.js
@@ -1,44 +1,16 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { mdsvex, escapeSvelte } from 'mdsvex'
-import { bundledLanguages, getSingletonHighlighter } from 'shiki'
-import rehypeUnwrapImages from 'rehype-unwrap-images'
-import remarkToc from 'remark-toc'
-import rehypeSlug from 'rehype-slug'
-
-const mdsvexOptions = {
-	extensions: ['.md'],
-	highlight: {
-		highlighter: async (/** @type {string} */ code, lang = 'text') => {
-			const highlighter = await getSingletonHighlighter({
-				themes: ['tokyo-night'],
-				langs: Object.keys(bundledLanguages)
-			})
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'tokyo-night' }))
-			return html
-		}
-	},
-	remarkPlugins: [rehypeUnwrapImages, [remarkToc, { tight: true }]],
-	rehypePlugins: [rehypeSlug]
-}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	extensions: ['.svelte'],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		adapter: adapter({
 			pages: 'public',
-			assets: 'public',
-			fallback: null,
-			precompress: false
-		}),
-		prerender: {
-			entries: ['*']
-		}
+			assets: 'public'
+		})
 	}
 }
 
