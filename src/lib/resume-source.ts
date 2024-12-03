00fcf1ea5c50cf06eca063fbde489c94cc3ce7f4
AliTaee <ali.taee.72@gmail.com>
2024-12-03 01:00:19 +0100

    docs(resume): add new resume

--- src/lib/resume-source.ts
+++ src/lib/resume-source.ts
@@ -29,7 +29,7 @@ export const seoTags = {
 }
 
 export const skills = [
-	'HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'React', 'Vue', 'Svelte', 'Next.Js', 'NodeJs', 'Code review', 'Team work', 'Critical thinking', 'SOLID', 'Redux',
+	'HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'React', 'Vue', 'Svelte', 'Three.js', 'Next.Js', 'NodeJs', 'Code review', 'Team work', 'Critical thinking', 'SOLID', 'Redux',
 	'Redux-Toolkit', 'React-Query', 'Git', 'E2E Testing', 'Unit Testing', 'Jest', 'Testing Library', 'Cypress', 'Playwright', 'Storybook', 'Responsive Design',
 	'Jira', 'Scrum', 'Agile Development', 'Socket.IO', 'REST API', 'Parcel.Js', 'Webpack', 'Rollup', 'Vite', 'Yarn', 'NPM'
 ]
