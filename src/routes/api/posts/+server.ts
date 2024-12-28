f26ab8c9ecd5663f22eff07d265bf604ed67b693
AliTaee <ali.taee.72@gmail.com>
2024-12-28 13:18:41 +0100

    refactor: organizer post into years folder

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,15 +3,16 @@ import { json } from "@sveltejs/kit"
 async function getPosts() {
     let posts: Post[] = []
 
-    const paths = import.meta.glob("/src/posts/*.md", { eager: true })
+    const paths = import.meta.glob("/src/posts/**/*.md", { eager: true })
 
     for (const path in paths) {
         const file = paths[path]
         const slug = path.split("/").at(-1)?.replace(".md", "")
+        const year = path.split("/").at(-2)
 
         if (file && typeof file === "object" && "metadata" in file && slug) {
             const metadata = file.metadata as Omit<Post, "slug">
-            const post = { ...metadata, slug } satisfies Post
+            const post = { ...metadata, slug: `${year}/${slug}` } satisfies Post
             if (post.published) {
                 posts.push(post)
             }
