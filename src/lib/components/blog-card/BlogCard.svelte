ded9666d40ee61e09e38aa62e4c9a09aa901d26b
AliTaee <ali.taee.72@gmail.com>
2024-12-28 20:18:08 +0100

    refactor blog card code to use js destructuring

--- src/lib/components/blog-card/BlogCard.svelte
+++ src/lib/components/blog-card/BlogCard.svelte
@@ -2,27 +2,28 @@
 	import { formatDate } from '$lib/utils.js'
 
 	export let post
+	const { slug, title, description, date, image, imageAlt } = post
 </script>
 
-{#key post.slug}
+{#key slug}
 	<div class="card bg-primary shadow-xl">
-		{#if post.image}
+		{#if image}
 			<figure class="h-60">
-				<img src={post.image} alt={post.imageAlt} />
+				<img src={image} alt={imageAlt} />
 			</figure>
 		{/if}
 		<div class="card-body">
 			<h2 class="gradient-heading" data-toc-ignore>
-				{post.title}
+				{title}
 			</h2>
-			<p class="text-white">{post.description}</p>
+			<p class="text-white">{description}</p>
 			<footer class="flex justify-start items-center py-4 text-white">
 				<div class="flex-auto flex justify-between items-center">
-					<small>On {formatDate(post.date)}</small>
+					<small>On {formatDate(date)}</small>
 				</div>
 			</footer>
 			<div class="card-actions justify-end">
-				<a class="btn btn-secondary" href={`/blog/${post.slug}`}> Read More </a>
+				<a class="btn btn-secondary" href={`/blog/${slug}`}> Read More </a>
 			</div>
 		</div>
 	</div>
