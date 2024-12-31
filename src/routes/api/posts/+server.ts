12cb8266da43cd1e883e3d0936f109962b57afc7
Ali Taee <32999571+AliTaee@users.noreply.github.com>
2024-12-31 17:14:17 +0100

    implement reading position indicator (#47)
    * build(styles): remove scss and install daisyui
    
    * refactor(toggle-theme): improve folder and component name
    
    * build(project setup): add configs for blog
    
    * refactor(project structure): improve project structure
    
    * build(setup): change project config to use static site generator
    
    * fix static site generator build
    
    * test fixing speed and loading pages without blog
    
    * fix(layout): move file to correct place
    
    * update now page
    
    * moving sort logic to featch markdown posts
    
    * add reading position indicator to blog posts

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,12 +3,7 @@ import { json } from "@sveltejs/kit"
 
 export async function GET() {
     const allPosts = await fetchMarkdownPosts();
-
-    const sortedPosts = allPosts.sort((firstPost, secondPost) => {
-        return new Date(secondPost.meta.date).getTime() - new Date(firstPost.meta.date).getTime();
-    });
-
-    return json(sortedPosts)
+    return json(allPosts)
 }
 
 export const prerender = true
